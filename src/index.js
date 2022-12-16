f49f6dca32b628b528056590f07799b62286595f
Veganizmas <linasmackevicius1997@gmail.com>
2022-12-16 11:36:35 +0200

    asd

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import AppLogin from "./AppLogin";
 import "../src/index.css";
-import SecurityApp from "./security/SecurityApp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
